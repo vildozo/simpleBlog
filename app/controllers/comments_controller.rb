6ab10cb8573baa1a2e33ee1cd617883c8a530d1b
vildozo <vildozo@outlook.com>
2019-11-07 01:06:18 -0400

    con autentificacion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
+	http_basic_authenticate_with name: "user", password: "1234", only: [:destroy]
 
 	def create
 		@post = Post.find(params[:post_id])
 		@comment=@post.comments.create(comment_params)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+
 		redirect_to post_path(@post)
 	end
 
